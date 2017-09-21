eda2c752cb93224147690bd56b0ac940b8259f4a
Lars Huring <lars.huring@gmail.com>
2017-09-22 00:34:01 +0200

    .

--- Domains/Session/session.shot.ts
+++ Domains/Session/session.shot.ts
@@ -5,6 +5,26 @@ export class Shot extends SessionString {
     public ShotTime: Date;
     public UserID: Number;
     public ShotMedia: String;
+    public shotMetadata: Array<ShotMetadataItem>;
 }
 
 let s = new Shot();
+
+enum Metadata {
+    TEMPERATURE,
+    WINDSPEED,
+    PRESSURE
+};
+
+class ShotMetadataItem {
+    public Type: Metadata;
+    public Payload: {key:Number, value:String};
+}
+
+let sm = new ShotMetadataItem();
+
+sm.Type = Metadata.TEMPERATURE;
+sm.Payload = {
+    key: 1,
+    value: 'foo'
+};
